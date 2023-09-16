68891a902fedbf81c74a30fe07823c97c6ef0543
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2023-09-16 10:12:05 -0400

    Update setup.sh

--- setup/pi/setup.sh
+++ setup/pi/setup.sh
@@ -3,7 +3,19 @@ sudo dpkg-reconfigure cloud-init
 sudo rm -rf /etc/cloud/ && sudo rm -rf /var/lib/cloud/
 sudo raspi-config nonint do_hostname flow-$EPOCHSECONDS
 sudo apt update && sudo apt upgrade -y
-sudo apt install network-manager python3-pip python3-venv python3-dev net-tools i2c-tools raspi-config python3-setuptools bluez libbluetooth-dev bluetooth libbluetooth-dev bluez-test-scripts bluez-tools python3-bluez -y
+sudo apt install pipx network-manager python3-pip python3-venv python3-dev net-tools i2c-tools raspi-config python3-setuptools bluez libbluetooth-dev bluetooth libbluetooth-dev bluez-test-scripts bluez-tools python3-bluez -y
+
+sudo pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
+sudo pip3 install RPi.GPIO
+
+mkdir /etc/systemd/system/bluetooth.service.d
+vi /etc/systemd/system/bluetooth.service.d/override.conf
+
+with the following content:
+
+[Service]
+ExecStart=
+ExecStart=/usr/lib/bluetooth/bluetoothd -C
 
 bluetoothctl power off
 echo "* 0000" > /tmp/bluetooth.cfg
